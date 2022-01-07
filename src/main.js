f8de30a4a740a6976ccd6acfab5954db3e945468
phongdz2002 <phongnvph18006@fpt.edu.vn>
2022-01-07 10:48:14 +0700

    fix loi 3

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { menus } from "./menu";
 function show_menu() {
   if (menus) {
     const menu = menus.map((item) => ` <li class=""><a href="" class=" text-2xl text-white block py-4 px-3 hover:underline hover:bg-white hover:text-black hover:ease-in-out duration-500" >${item}</a></li>`);
-    document.getElementById("menu").innerHTML = menu;
+    document.getElementById("menu").innerHTML = menu.join(" ");
   }
 }
 function show_product() {
