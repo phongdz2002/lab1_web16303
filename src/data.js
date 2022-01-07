5262cbc35fd3a29cdebdd99df76bd58871ac832a
phongdz2002 <phongnvph18006@fpt.edu.vn>
2022-01-07 10:38:21 +0700

    fix loi anh

--- src/data.js
+++ src/data.js
@@ -4,19 +4,19 @@ export const productList = [
         id: 1,
         name: "Vinh danh cao đẳng fpt",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
-        img: "img/product.jpg",
+        img: "https://picsum.photos/400/300",
     },
     {
         id: 2,
         name: "Vinh danh",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
-        img: "img/product.jpg",
+        img: "https://picsum.photos/400/300",
     },
     {
         id: 3,
         name: "Vinh danh",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
-        img: "img/product.jpg",
+        img: "https://picsum.photos/400/300",
     },
 ];
 export const productPost = [
@@ -24,18 +24,18 @@ export const productPost = [
         id: 1,
         name: "Vinh danh cao đẳng fpt",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
-        img: "img/product.jpg",
+        img: "https://picsum.photos/400/300",
     },
     {
         id: 2,
         name: "Vinh danh",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
-        img: "img/product.jpg",
+        img: "https://picsum.photos/400/300",
     },
     {
         id: 3,
         name: "Vinh danh",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
-        img: "img/product.jpg",
+        img: "https://picsum.photos/400/300",
     },
 ];
