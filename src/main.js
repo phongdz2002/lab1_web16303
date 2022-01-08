178bfae3a8361aaa9baf913b729d9f9263bb279c
phongdz2002 <phongnvph18006@fpt.edu.vn>
2022-01-08 15:51:55 +0700

    fix lan 13

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ router.on(
     {
         // eslint-disable-next-line no-return-assign
         "/": () => document.getElementById("app").innerHTML = homeHtml.print(),
+        // eslint-disable-next-line no-return-assign
+        "/about": () => document.getElementById("app").innerHTML = "html",
     },
 );
 router.resolve();
