14b097bb13d7801852c00d92dbd6771d3e017c7d
phongdz2002 <phongnvph18006@fpt.edu.vn>
2022-01-08 08:18:25 +0700

    fix lan 10

--- src/data.js
+++ src/data.js
@@ -2,19 +2,19 @@
 export const productList = [
     {
         id: 1,
-        name: "Vinh danh cao đẳng fpt",
+        name: "Kênh YouTube ViruSs bỗng biến mất loạt video thanh minh về drama lùa gà, chuyện gì đã xảy ra?",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
         img: "https://picsum.photos/400/300",
     },
     {
         id: 2,
-        name: "Vinh danh",
+        name: "Kênh YouTube ViruSs bỗng biến mất loạt video thanh minh về drama lùa gà, chuyện gì đã xảy ra?",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
         img: "https://picsum.photos/400/300",
     },
     {
         id: 3,
-        name: "Vinh danh",
+        name: "Kênh YouTube ViruSs bỗng biến mất loạt video thanh minh về drama lùa gà, chuyện gì đã xảy ra?",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
         img: "https://picsum.photos/400/300",
     },
@@ -22,19 +22,19 @@ export const productList = [
 export const productPost = [
     {
         id: 1,
-        name: "Vinh danh cao đẳng fpt",
+        name: "Kênh YouTube ViruSs bỗng biến mất loạt video thanh minh về drama lùa gà, chuyện gì đã xảy ra?",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
         img: "https://picsum.photos/400/300",
     },
     {
         id: 2,
-        name: "Vinh danh",
+        name: "Kênh YouTube ViruSs bỗng biến mất loạt video thanh minh về drama lùa gà, chuyện gì đã xảy ra?",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
         img: "https://picsum.photos/400/300",
     },
     {
         id: 3,
-        name: "Vinh danh",
+        name: "Kênh YouTube ViruSs bỗng biến mất loạt video thanh minh về drama lùa gà, chuyện gì đã xảy ra?",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolorum, in optio at excepturi aspernatur praesentium error ducimus commodi tenetur laborum velit! Deleniti expedita aut at, doloremque consequatur magnam asperiores?",
         img: "https://picsum.photos/400/300",
     },
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { menus } from "./menu";
 
 function show_menu() {
   if (menus) {
-    const menu = menus.map((item) => ` <li class=""><a href="" class=" text-white block py-3 px-2 hover:underline hover:bg-white hover:text-black hover:ease-in-out duration-500" >${item}</a></li>`);
+    const menu = menus.map((item) => ` <li class=""><a href="" class=" text-white block py-3 px-2  hover:bg-white hover:text-black hover:ease-in-out duration-500" >${item}</a></li>`);
     document.getElementById("menu").innerHTML = menu.join(" ");
   }
 }
@@ -12,7 +12,7 @@ function show_product() {
     const product = productList.map((item) => `  <div class="border-solid border-2 border-gray-600 text-center m-3 py-2">
        <div class="overflow-hidden h-60 w-96 m-auto"> <img src="${item.img}" alt="" class=" 	 hover:scale-110 hover:translate-y-2 hover:ease-in-out duration-500"></div>
         <div class="p-5">
-          <h3><a href="" class="hover:underline font-bold text-red-600 text-2xl hover:ease-in-out duration-500 hover:text-blue-600">${item.name}</a></h3>
+          <h3 class="my-2"><a href="" class="hover:underline font-bold text-red-600 text-2xl hover:ease-in-out duration-500 hover:text-blue-600">${item.name}</a></h3>
         <p class="text-left">${item.desc}</p>
         </div>
       </div>`);
@@ -25,7 +25,7 @@ function show_post() {
     const product = productPost.map((item) => `  <div class="border-solid border-2 border-indigo-600 text-center m-3 py-2">
     <div class="overflow-hidden h-60 w-96 m-auto"> <img src="${item.img}" alt="" class=" 	 hover:scale-110 hover:translate-y-2 hover:ease-in-out duration-500"></div>
         <div class="p-5">
-          <h3><a href="" class="hover:underline font-bold text-red-600 text-2xl hover:ease-in-out duration-500 hover:text-blue-600">${item.name}</a></h3>
+          <h3 class="my-2"><a href="" class="hover:underline font-bold text-red-600 text-2xl hover:ease-in-out duration-500 hover:text-blue-600">${item.name}</a></h3>
         <p class="text-left">${item.desc}</p>
         </div>
       </div>`);
