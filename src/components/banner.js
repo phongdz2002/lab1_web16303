43534504d1b86fc78d02abd7b23e8b05489ea267
phongdz2002 <phongnvph18006@fpt.edu.vn>
2022-01-08 15:56:12 +0700

    sửa đường dẫn ảnh

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const homeBanner = {
     print: () => `<div class="mt-2">
-    <img src="./img/banner.jpg" alt="" class="w-full">
+    <img src="img/banner.jpg" alt="" class="w-full">
   </div>`,
 };
