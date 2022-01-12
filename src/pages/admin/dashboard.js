deb42bbe909c808a78394f4ea56a77958ab8e1b7
phongdz2002 <naruto2003v@gmail.com>
2022-01-12 08:30:00 +0700

    sửa lại đường dẫn templates

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -5,7 +5,7 @@ export const HomeAdmin = {
       <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="https://demos.creative-tim.com/soft-ui-dashboard/pages/dashboard.html" target="_blank">
-          <img src="../soft-ui-dashboard-main/assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
+          <img src="./soft-ui-dashboard-main/assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
           <span class="ms-1 font-weight-bold">FPT POLYTECHNIC</span>
         </a>
       </div>
@@ -184,7 +184,7 @@ export const HomeAdmin = {
       </div>
       <div class="sidenav-footer mx-3 ">
         <div class="card card-background shadow-none card-background-mask-secondary" id="sidenavCard">
-          <div class="full-background" style="background-image: url('../soft-ui-dashboard-main/assets/img/curved-images/white-curved.jpeg')"></div>
+          <div class="full-background" style="background-image: url('./soft-ui-dashboard-main/assets/img/curved-images/white-curved.jpeg')"></div>
           <div class="card-body text-start p-3 w-100">
             <div class="icon icon-shape icon-sm bg-white shadow text-center mb-3 d-flex align-items-center justify-content-center border-radius-md">
               <i class="ni ni-diamond text-dark text-gradient text-lg top-0" aria-hidden="true" id="sidenavCardIcon"></i>
@@ -247,7 +247,7 @@ export const HomeAdmin = {
                     <a class="dropdown-item border-radius-md" href="javascript:;">
                       <div class="d-flex py-1">
                         <div class="my-auto">
-                          <img src="../soft-ui-dashboard-main/assets/img/team-2.jpg" class="avatar avatar-sm  me-3 ">
+                          <img src="./soft-ui-dashboard-main/assets/img/team-2.jpg" class="avatar avatar-sm  me-3 ">
                         </div>
                         <div class="d-flex flex-column justify-content-center">
                           <h6 class="text-sm font-weight-normal mb-1">
@@ -265,7 +265,7 @@ export const HomeAdmin = {
                     <a class="dropdown-item border-radius-md" href="javascript:;">
                       <div class="d-flex py-1">
                         <div class="my-auto">
-                          <img src="../soft-ui-dashboard-main/assets/img/small-logos/logo-spotify.svg" class="avatar avatar-sm bg-gradient-dark  me-3 ">
+                          <img src="./soft-ui-dashboard-main/assets/img/small-logos/logo-spotify.svg" class="avatar avatar-sm bg-gradient-dark  me-3 ">
                         </div>
                         <div class="d-flex flex-column justify-content-center">
                           <h6 class="text-sm font-weight-normal mb-1">
